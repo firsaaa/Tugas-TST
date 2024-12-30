ff8fda28eda1631d3c488acf982a3fdf6d3e6d72
firsaaa <firsa.raissa@gmail.com>
2024-12-30 21:39:37 +0700

    update

--- BE/app/database.py
+++ BE/app/database.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
+# Load environment variables
 load_dotenv()
 
 DATABASE_URL = os.getenv('DATABASE_URL')
@@ -11,10 +12,17 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set. Check your .env file.")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True, 
+    connect_args={"sslmode": "require"}  
+)
+
+# Session dan Base untuk ORM
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+# Dependency untuk mendapatkan session database
 def get_db():
     db = SessionLocal()
     try:
