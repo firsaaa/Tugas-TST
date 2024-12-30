48c840c3d3ff0ea998aad3c4e52edf7133b6682e
firsaaa <firsa.raissa@gmail.com>
2024-12-30 22:55:57 +0700

    bs dong

--- BE/app/database.py
+++ BE/app/database.py
@@ -5,27 +5,20 @@ from dotenv import load_dotenv
 import os
 import logging
 
+# Load environment variables
+load_dotenv()
+
+
 logging.basicConfig(level=logging.INFO)
 logging.info(f"Connecting to database: {os.getenv('DATABASE_URL')}")
 
 engine = create_engine(
     os.getenv("DATABASE_URL"),
-    connect_args={"sslmode": "require"}
 )
-
-
-# Load environment variables
-load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set. Check your .env file.")
-
-# Create engine with SSL enabled
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"sslmode": "require"}  # Important for Supabase
-)
 
 # Session and Base for ORM
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
