271ff17f17897dc9b0f9b7772e93719366917a57
firsaaa <firsa.raissa@gmail.com>
2025-01-05 15:30:07 +0700

    check debug

--- BE/app/routes/public.py
+++ BE/app/routes/public.py
@@ -1,6 +1,15 @@
 from fastapi import APIRouter
+import os
 
 router = APIRouter()
+
+@router.get("/debug")
+def debug_env():
+    return {
+        "DATABASE_URL": os.getenv("DATABASE_URL"),
+        "API_KEY": os.getenv("API_KEY"),
+        "SECRET_KEY": os.getenv("SECRET_KEY")
+    }
 
 @router.get("/")
 def read_public():
