eaa69999dbab9701e83eda10845c568351fb9853
firsaaa <firsa.raissa@gmail.com>
2025-01-09 00:17:59 +0700

    add cors

--- BE/main.py
+++ BE/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500", 
+        "https://coworkingspace-six.vercel.app/",
         "https://coworkingspace-backend.vercel.app"  \
     ],
     allow_credentials=True,
